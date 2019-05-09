4dda449346bc89a4ed45af12e3d50c9780d3d5db
Mario Adamo Jr <marioadamojr@gmail.com>
2019-05-09 23:05:28 +0000

    Basic Ground Work

--- lib/BCFF/cli.rb
+++ lib/BCFF/cli.rb
@@ -2,4 +2,20 @@
 
 class BCFF::CLI
 
+  def call
+    greeting
+    tiers
+    goodbye
+  end
+
+  def greeting
+  end
+
+  def tiers
+  end
+
+  def goodbye
+    puts "Thank you for checking out my CLI!"
+  end
+
 end
