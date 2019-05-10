efc2f55fb88edbf59e4bd2b41608b7c1f24add98
Mario Adamo Jr <marioadamojr@gmail.com>
2019-05-10 00:50:57 +0000

    Update 1, trying to get CLI to work

--- lib/BCFF/cli.rb
+++ lib/BCFF/cli.rb
@@ -9,13 +9,32 @@ class BCFF::CLI
   end
 
   def greeting
+    @positions = []
+
   end
 
   def tiers
+    input = nil
+    while input != "exit"
+      puts "Enter the position you want to see the tier list for, or type exit to close the program."
+      input = gets.strip.downcase
+
+      if input == "qb"
+        puts "Here are #{input}s"
+        scrape_"#{input}"
+      elsif input == "rb"
+        puts "#{input}"
+      else
+        "Please enter a valid position: qb, rb, wr, te, flex, k, def, or exit"
+      end
+    end
   end
 
   def goodbye
     puts "Thank you for checking out my CLI!"
+  end
+
+  def scrape_qb
   end
 
 end
